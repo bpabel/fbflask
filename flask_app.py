e34b3590645190166de259d500520958001eb65e
Brendan Abel <007brendan@gmail.com>
2014-10-06 17:09:00 -0700

    added privacy policy

--- flask_app.py
+++ flask_app.py
@@ -13,5 +13,10 @@ def index():
     return render_template('layout.html')
 
 
+@app.route('/privacy/')
+def privacy():
+	return render_template('privacy.html')
+	
+
 if __name__ == '__main__':
     app.run()
